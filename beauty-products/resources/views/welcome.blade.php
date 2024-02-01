e142db0491281c006a71ce98f2bac52af111f584
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-01 01:06:58 +0100

    modifier le boutton de suppression de produit et ajout des sweet alert pour la modification et la suppression

--- beauty-products/resources/views/welcome.blade.php
+++ beauty-products/resources/views/welcome.blade.php
@@ -443,11 +443,6 @@
                             class="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" type="button">
                         Add Product
                     </button>
-                    <!-- Modal toggle -->
-                    <button data-modal-target="crud-modal-category" data-modal-toggle="crud-modal-category"
-                            class="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" type="button">
-                        Add Category
-                    </button>
                     {{--                        <a class="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">Add Category</a>--}}
                     {{--                        <a class="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">Logout</a>--}}
                 </div>
@@ -567,11 +562,16 @@
                                 </svg>
                             </button>
 {{--                            delete button--}}
-                            <button class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-                                </svg>
-                            </button>
+                            <form id="delete-form-{{ $product->id }}" action="{{ url('/destroy', $product->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500" onclick="confirmDelete({{ $product->id }})">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                                    </svg>
+                                </button>
+                            </form>
+
 
                         </div>
                         <div class="px-5 py-3">
@@ -604,7 +604,7 @@
                                     </button>
                                 </div>
                                 <!-- Modal body -->
-                                <form action="/update" method="post" class="p-4 md:p-5">
+                                <form action="/update/{{ $product->id }}" method="post" class="p-4 md:p-5">
                                     @csrf
                                     <div class="grid gap-4 mb-4 grid-cols-2">
                                         <div class="col-span-2">
@@ -745,55 +745,6 @@
 </div>
 
 
-{{--    add category form--}}
-<!-- Main modal -->
-<div id="crud-modal-category" tabindex="-1" aria-hidden="true"
-     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                    Create New Category
-                </h3>
-                <button type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-toggle="crud-modal-category">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                         viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
-            <!-- Modal body -->
-            <form class="p-4 md:p-5">
-                <div class="grid gap-4 mb-4 grid-cols-2">
-                    <div class="col-span-2">
-                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-                        <input type="text" name="category" id="category"
-                               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                               placeholder="Type category name" required="">
-                    </div>
-                </div>
-                <button type="submit"
-                        class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                              clip-rule="evenodd"></path>
-                    </svg>
-                    Add new category
-                </button>
-            </form>
-        </div>
-    </div>
-</div>
-
-
 
 {{--alert de succes , product--}}
 @if (session('status'))
@@ -806,5 +757,25 @@
         });
     </script>
 @endif
-</body>
+@if (session('success'))
+    <script>
+
+        Swal.fire({
+            title: "Product Updating",
+            text: "{{session('success')}}",
+            icon: "success"
+        });
+    </script>
+@endif
+
+@if (session('delete'))
+    <script>
+
+        Swal.fire({
+            title: "Product Deleting",
+            text: "{{session('delete')}}",
+            icon: "success"
+        });
+    </script>
+@endif</body>
 </html>
