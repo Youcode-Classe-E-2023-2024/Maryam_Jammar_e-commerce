686cf6f2470001bf02e11e1731e752729e037732
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-04 01:54:53 +0100

    update products page

--- beauty-products/resources/views/welcome.blade.php
+++ beauty-products/resources/views/welcome.blade.php
@@ -422,25 +422,28 @@
                             </svg>
                         </a>
 
-                    <div class="flex sm:hidden">
-                        <button @click="isOpen = !isOpen" type="button"
-                                class="text-gray-600 hover:text-gray-500 focus:outline-none focus:text-gray-500"
-                                aria-label="toggle menu">
-                            <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
-                                <path fill-rule="evenodd"
-                                      d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                            </svg>
-                        </button>
-                    </div>
+{{--                    <div class="flex sm:hidden">--}}
+{{--                        <button @click="isOpen = !isOpen" type="button"--}}
+{{--                                class="text-gray-600 hover:text-gray-500 focus:outline-none focus:text-gray-500"--}}
+{{--                                aria-label="toggle menu">--}}
+{{--                            <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">--}}
+{{--                                <path fill-rule="evenodd"--}}
+{{--                                      d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>--}}
+{{--                            </svg>--}}
+{{--                        </button>--}}
+{{--                    </div>--}}
                 </div>
             </div>
 
             <div class="relative mt-6 max-w-lg mx-auto">
+                <h2 class="flex items-center justify-center p-2 font-bold">
+                    Sort by
+                </h2>
+
                 <div class='flex items-center justify-center'>
 
-                    <ul class="mx-auto grid max-w-full w-full grid-cols-3 gap-x-5 px-8">
+                    <ul class="mx-auto grid max-w-full w-full grid-cols-3 gap-x-5">
 
-                        Sort by
                         {{--                        <li class="">--}}
                         {{--                            <input class="peer sr-only" type="radio" value="yes" name="answer" id="yes" checked/>--}}
                         {{--                            <label--}}
@@ -482,14 +485,14 @@
 <main class="my-8">
     <div class="container mx-auto px-6">
         <div class="h-64 rounded-md overflow-hidden bg-cover bg-center"
-             style="background-image: url('https://images.unsplash.com/photo-1577655197620-704858b270ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1280&q=144')">
+             style="background-image: url('https://pirg.org/edfund/wp-content/uploads/2022/03/ecommerce-ge3efc47c1_1920_1.jpg')">
             <div class="bg-gray-900 bg-opacity-50 flex items-center h-full">
                 <div class="px-10 max-w-xl">
-                    <h2 class="text-2xl text-white font-semibold">Sport Shoes</h2>
+                    <h2 class="text-2xl text-white font-semibold">Start today ! </h2>
                     <p class="mt-2 text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
                         facere provident molestias ipsam sint voluptatum pariatur.</p>
                     <button
-                        class="flex items-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                        class="flex items-center mt-4 px-3 py-2 bg-green-700 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
                         <span>Get Started</span>
                         <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round"
                              stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
