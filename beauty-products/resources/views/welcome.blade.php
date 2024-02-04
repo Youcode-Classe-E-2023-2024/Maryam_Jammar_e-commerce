ad198cbde585abd1da1c2d97cdb27e631970c9e6
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-04 01:17:35 +0100

    update add button

--- beauty-products/resources/views/welcome.blade.php
+++ beauty-products/resources/views/welcome.blade.php
@@ -400,28 +400,27 @@
     <header>
         <div class="container mx-auto px-6 py-3">
             <div class="flex items-center justify-between">
-                <div class="hidden w-full text-gray-600 md:flex md:items-center">
-                    <svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                              d="M16.2721 10.2721C16.2721 12.4813 14.4813 14.2721 12.2721 14.2721C10.063 14.2721 8.27214 12.4813 8.27214 10.2721C8.27214 8.06298 10.063 6.27212 12.2721 6.27212C14.4813 6.27212 16.2721 8.06298 16.2721 10.2721ZM14.2721 10.2721C14.2721 11.3767 13.3767 12.2721 12.2721 12.2721C11.1676 12.2721 10.2721 11.3767 10.2721 10.2721C10.2721 9.16755 11.1676 8.27212 12.2721 8.27212C13.3767 8.27212 14.2721 9.16755 14.2721 10.2721Z"
-                              fill="currentColor"/>
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                              d="M5.79417 16.5183C2.19424 13.0909 2.05438 7.39409 5.48178 3.79417C8.90918 0.194243 14.6059 0.054383 18.2059 3.48178C21.8058 6.90918 21.9457 12.6059 18.5183 16.2059L12.3124 22.7241L5.79417 16.5183ZM17.0698 14.8268L12.243 19.8965L7.17324 15.0698C4.3733 12.404 4.26452 7.97318 6.93028 5.17324C9.59603 2.3733 14.0268 2.26452 16.8268 4.93028C19.6267 7.59603 19.7355 12.0268 17.0698 14.8268Z"
-                              fill="currentColor"/>
-                    </svg>
-                    <span class="mx-1 text-sm">NY</span>
-                </div>
-{{--                <div class="w-full text-gray-700 md:text-center text-2xl font-semibold">--}}
+                    <div data-modal-target="crud-modal" data-modal-toggle="crud-modal" type="button" class=" w-full text-gray-600 md:flex md:items-center hover:underline sm:mx-3 sm:mt-0">
+                       <button class="flex items-center">
+                           <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
+                           </svg>
+                           <span class="mx-1 text-sm ">Add Your Product</span>
+
+                       </button>
+                    </div>
+
+                {{--                <div class="w-full text-gray-700 md:text-center text-2xl font-semibold">--}}
                     <img src="/storage/picture/Beauty_Merry__2.png" alt="test" class="w-16">
 {{--                </div>--}}
                 <div class="flex items-center justify-end w-full">
-                    <button @click="cartOpen = !cartOpen" class="text-gray-600 focus:outline-none mx-4 sm:mx-0">
-                        <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                             viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                        </svg>
-                    </button>
+                        <a href="" class="flex">
+                            <span>Logout</span>
+                            <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                 viewBox="0 0 24 24" stroke="currentColor">
+                                <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                            </svg>
+                        </a>
 
                     <div class="flex sm:hidden">
                         <button @click="isOpen = !isOpen" type="button"
@@ -435,24 +434,13 @@
                     </div>
                 </div>
             </div>
-            <nav :class="isOpen ? '' : 'hidden'" class="sm:flex sm:justify-center sm:items-center mt-4">
-                <div class="flex flex-col sm:flex-row">
-                    {{--                        <a class="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">Home</a>--}}
-                    {{--                        <a class="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">Add Product</a>--}}
-                    <button data-modal-target="crud-modal" data-modal-toggle="crud-modal"
-                            class="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" type="button">
-                        Add Product
-                    </button>
-                    {{--                        <a class="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">Add Category</a>--}}
-                    {{--                        <a class="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">Logout</a>--}}
-                </div>
-            </nav>
+
             <div class="relative mt-6 max-w-lg mx-auto">
                 <div class='flex items-center justify-center'>
-                    Trier par
+
                     <ul class="mx-auto grid max-w-full w-full grid-cols-3 gap-x-5 px-8">
 
-
+                        Sort by
                         {{--                        <li class="">--}}
                         {{--                            <input class="peer sr-only" type="radio" value="yes" name="answer" id="yes" checked/>--}}
                         {{--                            <label--}}
@@ -488,16 +476,7 @@
             </div>
         </div>
     </header>
-    <div :class="cartOpen ? 'translate-x-0 ease-out' : 'translate-x-full ease-in'"
-         class="fixed right-0 top-0 max-w-xs w-32 mt-6  transition duration-300 transform overflow-y-auto">
-        <a class="flex items-center justify-center mt-8 px-3 py-3 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-            <span>Logout</span>
-            <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                 viewBox="0 0 24 24" stroke="currentColor">
-                <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
-            </svg>
-        </a>
-    </div>
+
 
 </div>
 <main class="my-8">
